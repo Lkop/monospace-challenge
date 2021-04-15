f0f3718e320806688cb9f162bbb0aa7575923a03
Loukas <37883287+Lkop@users.noreply.github.com>
2021-04-15 07:56:43 -0700

    Creating Vessel and Voyage models

--- app/Models/Vessel.php
+++ app/Models/Vessel.php
@@ -9,15 +9,5 @@ class Vessel extends Model
 {
 
 	public $table = 'vessels';
-
-    protected $fillable = [
-    	'name',
-    	'imo_number'
-    ];
-
-    protected $hidden = [
-    	'created_at',
-    	'updated_at'
-    ];
-
+	public $timestamps = false;
 }
--- app/Models/Voyage.php
+++ app/Models/Voyage.php
@@ -4,7 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Voyage extends Model
-{
+class Voyage extends Model {
+
 	public $table = 'voyages';
+	public $timestamps = false;
 }
